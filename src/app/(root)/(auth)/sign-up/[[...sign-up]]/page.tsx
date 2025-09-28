b0e2bb0bf7ee17a30318a1a2b78423f027a9aa0f
Ti0Elvis <elvis.veramijares@gmail.com>
2025-09-28 14:23:03 +0200

    Add sign-up page with redirect handling and loading state; Create BackgroundPattern and LoadingSpinner components

--- src/app/(root)/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(root)/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 export default function Page() {
   return (
     <MaxWidthWrapper className="w-full min-h-[calc(100vh-4rem)] flex items-center justify-center">
-      <SignUp />
+      <SignUp fallbackRedirectUrl="/welcome" forceRedirectUrl="/welcome" />
     </MaxWidthWrapper>
   );
 }
